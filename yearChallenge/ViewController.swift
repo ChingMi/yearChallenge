3532c56e3368b5480d59fe05c66a86517ed9f714
GoldenRice <bluesky20242@gmail.com>
2022-03-26 14:17:05 +0800

    Initial Commit

---
+++ yearChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yearChallenge
+//
+//  Created by 黃靖蜜 on 2022/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
